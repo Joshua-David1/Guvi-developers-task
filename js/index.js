f36b15bbae3e84b3e6abd433dafe2276a5d0cbdf
Joshua <joshuadavid432@gmail.com>
2023-04-09 23:03:17 +0530

    Update Home Page

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$(".sign-up-btn").click(function(){
+		window.location.href = "http://localhost:5000/register.html";
+	});
+
+	$(".sign-in-btn").click(function(){
+		window.location.href = "http://localhost:5000/login.html";
+	})
+
+})
